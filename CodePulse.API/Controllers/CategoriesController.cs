2de4961617d4b41caa5954f73b0fb04d18a44200
Szymon Fornal <szymekfornal6@gmail.com>
2024-07-01 12:24:49 +0200

    CRUD for categories

--- CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -25,7 +25,7 @@ namespace CodePulse.API.Controllers
                 UrlHandle = request.UrlHandle,
             };
 
-           await _categoryRepository.CreateAsync(category);
+            await _categoryRepository.CreateAsync(category);
 
             var response = new CategoryDto
             {
@@ -43,16 +43,87 @@ namespace CodePulse.API.Controllers
             var categories = await _categoryRepository.GetAllAsync();
 
             var response = new List<CategoryDto>();
-            foreach(var categoryDto in categories)
+            foreach (var categoryDto in categories)
             {
                 response.Add(new CategoryDto
                 {
-                    Id =categoryDto.Id,
+                    Id = categoryDto.Id,
                     Name = categoryDto.Name,
                     UrlHandle = categoryDto.UrlHandle
                 });
             }
             return Ok(response);
         }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetCategoryById([FromRoute] Guid id)
+        {
+            var existingCategory = await _categoryRepository.GetByIdAsync(id);
+            if (existingCategory is null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = existingCategory.Id,
+                Name = existingCategory.Name,
+                UrlHandle = existingCategory.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> EditCategory([FromRoute] Guid id, UpdateCategoryRequestDto request)
+        {
+            var category = new Category
+            {
+                Id = id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await _categoryRepository.UpdateAsync(category);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteCategory([FromRoute] Guid id)
+        {
+            var category = await _categoryRepository.DeleteAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+
     }
 }
--- CodePulse.API/Repositories/Implementation/CategoryRepository.cs
+++ CodePulse.API/Repositories/Implementation/CategoryRepository.cs
@@ -20,9 +20,41 @@ namespace CodePulse.API.Repositories.Implementation
             return category;
         }
 
+        public async Task<Category?> DeleteAsync(Guid id)
+        {
+           var existingCategory = await _dbContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if(existingCategory is null)
+            {
+                return null;
+            }
+
+            _dbContext.Categories.Remove(existingCategory);
+            await _dbContext.SaveChangesAsync();
+            return existingCategory;
+        }
+
         public async Task<IEnumerable<Category>> GetAllAsync()
         {
             return await _dbContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Category?> UpdateAsync(Category category)
+        {
+            var existingCategory = await _dbContext.Categories.FirstOrDefaultAsync(x => x.Id == category.Id);
+            
+            if(existingCategory != null)
+            {
+                _dbContext.Entry(existingCategory).CurrentValues.SetValues(category);
+                await _dbContext.SaveChangesAsync();
+                return category;
+            }
+
+            return null;
         }
     }
 }
