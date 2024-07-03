56eb9f1c60b6631562dc9aa915abbab914dce703
Szymon Fornal <szymekfornal6@gmail.com>
2024-07-03 13:44:48 +0200

    create add blogspot

---
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -0,0 +1,52 @@
+﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository _blogPostRepository;
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Title = request.Title,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                ShortDescription = request.ShortDescription,
+                UrlHandle = request.UrlHandle,
+                Content = request.Content,
+                PublishedDate = request.PublishedDate
+            };
+
+            blogPost = await _blogPostRepository.CreateAsync(blogPost);
+
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = request.Author,
+                Content = request.Content,
+                PublishedDate = request.PublishedDate,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                UrlHandle = request.UrlHandle,
+                ShortDescription = request.ShortDescription,
+                IsVisible = request.IsVisible,
+                Title = request.Title
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ CodePulse.API/Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,23 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Repositories.Interface;
+
+namespace CodePulse.API.Repositories.Implementation
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public BlogPostRepository(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public async Task<BlogPost> CreateAsync(BlogPost blogPost)
+        {
+            await _dbcontext.BlogPosts.AddAsync(blogPost);
+            await _dbcontext.SaveChangesAsync();
+            
+            return blogPost;
+        }
+    }
+}
